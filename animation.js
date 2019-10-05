0cb13a0c7d4207815792d720af8cedf4a15294af
Hazel Prasetya <hprasetya@Ekos-MacBook-Air-2.local>
2019-10-05 11:59:37 -0700

    updated

--- animation.js
+++ animation.js
@@ -1,5 +1,6 @@
 import * as THREE from './three/build/three.module.js';
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -9,16 +10,43 @@ var state = 'zoom';
 
 // Set up scene
 var scene = new THREE.Scene();
-//scene.fog = new THREE.Fog(0x000000, 0, 20000);
+scene.background = new THREE.Color(0x000000);
+
 var origin = new THREE.Vector3(0, 0, 0);
+var xAxis = new THREE.Vector3(1, 0, 0);
+var yAxis = new THREE.Vector3(0, 1, 0);
+var zAxis = new THREE.Vector3(0, 0, 1);
+
+
+var dist = 2;
+var theta;
+var phi;
+var light;
+for (var i = 0; i < 30; i ++) {
+    theta = Math.random() * Math.PI * 2;
+    phi = Math.random() * Math.PI;
+
+    var light = new THREE.PointLight(0xffffff, 0.5, 10000);
+    light.position.set(0, 0, dist);
+    light.position.applyAxisAngle(xAxis, theta);
+    light.position.applyAxisAngle(yAxis, phi);
+
+    scene.add(light);
+}
 
 const whratio = window.innerWidth/window.innerHeight;
-var camera = new THREE.OrthographicCamera(-3000 * whratio, 3000 * whratio, -3000, 3000, -5000, 64000);
+var camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight, 0.01, 10000000);
+var cameraStart = new THREE.Vector3(10500, -2500, 1000);
+camera.position.copy(cameraStart);
+//camera.up = new THREE.Vector3(-0.75, -0.1, 1);
+camera.up = new THREE.Vector3(0, 0, 1);
 
 var controls = new OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
+controls.enableKeys = false;
 
-camera.position.set(-100, 2500, 1000);
+//camera.position.set(0, 0, -1000);
+console.log(camera);
 controls.update()
 
 var maxZoomFrames = 120;
@@ -26,61 +54,67 @@ var zoomFrames = 0;
 var zoomCounter = 0;
 var zoomPosStart = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
 var zoomPosEnd = new THREE.Vector3(215, 0, 0);
+//var zoomPosEnd = new THREE.Vector3(0, 0, 0);
 var zoomPosDifferential = new THREE.Vector3();
 var zoomViewStart = new THREE.Vector3(0, 0, 0);
 var zoomViewEnd = new THREE.Vector3(215, 0, 0);
+//var zoomViewEnd = new THREE.Vector3(0, 0, 0);
 var zoomViewCurr = new THREE.Vector3(0, 0, 0);
 var zoomViewDifferential = new THREE.Vector3();
-var zoomZoomStart = camera.zoom;
-var zoomZoomEnd = 150;
-
-var zoom2Start = zoomZoomEnd;
-var zoom2End = 100000;
 
 var zoomIn = true;
 
 var solarSystemSetup = [
     {
+        name: 'Sun',
         x: 0,
         y: 0,
         z: 0
     },
     {
+        name: 'Mercury',
         x: 83,
         y: 0,
         z: 0
     },
     {
+        name: 'Venus',
         x: 156,
         y: 0,
         z: 0
     },
     {
+        name: 'Earth',
         x: 215,
         y: 0,
         z: 0
     },
     {
+        name: 'Mars',
         x: 328,
         y: 0,
         z: 0
     },
     {
+        name: 'Jupiter',
         x: 1119,
         y: 0,
         z: 0
     },
     {
+        name: 'Saturn',
         x: 2061,
         y: 0,
         z: 0
     },
     {
+        name: 'Uranus',
         x: 4128,
         y: 0,
         z: 0
     },
     {
+        name: 'Neptune',
         x: 6463,
         y: 0,
         z: 0
@@ -106,125 +140,70 @@ for (var i in solarSystemSetup) {
     }
 }
 
-// temp sun
-var tempsun = new THREE.Mesh(new THREE.SphereGeometry(1, 10), new THREE.MeshBasicMaterial({color: 0xffff00}));
-tempsun.position.set(0, 0, 0);
-scene.add(tempsun);
+let loader = new GLTFLoader();
+loader.load('./CAC Planets/Milky Way/scene.gltf', function(draw) {
+    var bg = draw.scene.children[0];
+    bg.scale.set(100, 100, 100);
+    scene.add(draw.scene);
+});
+loader.load('./CAC Planets/Earth/scene.gltf', function(draw){
+    var earth = draw.scene.children[0];
+    var scale = 0.0088675;
+    earth.scale.set(scale, scale, scale);
+    earth.position.set(215-scale, -scale * 0.8125, -scale);
+    scene.add(draw.scene);
+});
 
-// temp earth
-var tempearth = new THREE.Mesh(new THREE.SphereGeometry(0.0092, 10), new THREE.MeshBasicMaterial({color: 0x00ffff}));
-tempearth.position.set(215, 0, 0);
-scene.add(tempearth);
+loader.load('./CAC Planets/Sun/scene.gltf', function(draw){
+    var earth = draw.scene.children[0];
+    var scale = 0.09639;
+    earth.scale.set(scale, scale, scale);
 
-var xAxis = new THREE.Vector3(1, 0, 0);
-var yAxis = new THREE.Vector3(0, 1, 0);
-var zAxis = new THREE.Vector3(0, 0, 1);
-
-var stars = [];
-var starColors = [ 
-    0xffffff, // white
-    0xffaf7f, // orange
-    0xffff7f, // yellow
-    0xff7f7f, // red
-    0x7f7fff, // blue
-];
-var starGeometry = new THREE.SphereGeometry(6, 5, 5);
-var starMaterial;
-
-var dist;
-var theta;
-var phi;
-var star;
-for (var i = 0; i < 8000; i ++) {
-    starMaterial = new THREE.MeshBasicMaterial({color: starColors[Math.floor(Math.random() * 5)]});
-
-    dist = 16000;
-    theta = Math.random() * Math.PI * 2;
-    phi = Math.random() * Math.PI;
-
-    star = new THREE.Mesh(starGeometry, starMaterial);
-    star.position.set(0, 0, dist);
-    star.position.applyAxisAngle(xAxis, theta);
-    star.position.applyAxisAngle(yAxis, phi);
-
-    stars.push(star);
-    //scene.add(star);
-}
+    scene.add(draw.scene);
+    animate();
+});
 
 var cameraVector = camera.getWorldDirection(cameraVector);
+
+function bellCurve(curr, max) {
+    var map = (curr / max) * 6 - 3;
+    return Math.exp(-map * map) / Math.sqrt(Math.PI);
+}
 function animate() {
     //for (var obj in 
 
     if (state == 'zoom') {
+        controls.enableRotate = false;
         zoomFrames ++;
-        zoomCounter = Math.sin(zoomFrames * Math.PI / maxZoomFrames);
+        zoomCounter = bellCurve(zoomFrames, maxZoomFrames);
 
         zoomPosDifferential.subVectors(zoomPosEnd, zoomPosStart);
-        zoomPosDifferential.multiplyScalar(zoomCounter * Math.PI / 2 / maxZoomFrames);
+        zoomPosDifferential.multiplyScalar(zoomCounter * 6 / maxZoomFrames * /*0.9995*/1.00001875);
         zoomViewDifferential.subVectors(zoomViewEnd, zoomViewStart);
-        zoomViewDifferential.multiplyScalar(zoomCounter * Math.PI / 2  / maxZoomFrames);
+        zoomViewDifferential.multiplyScalar(zoomCounter * 6 / maxZoomFrames);
         
         camera.position.add(zoomPosDifferential);
         zoomViewCurr.add(zoomViewDifferential);
 
         controls.target = zoomViewCurr;
 
-        camera.zoom += (zoomZoomEnd - zoomZoomStart) * zoomCounter * Math.PI / 2  / maxZoomFrames;
-        camera.updateProjectionMatrix();
-
         if (zoomFrames >= maxZoomFrames) {
             zoomViewCurr.copy(zoomViewEnd);
-            controls.target = zoomViewCurr;
+
+            //camera.position.copy(zoomPosEnd);
+            controls.target = zoomViewEnd;
 
             zoomFrames = 0;
-            camera.zoom = zoomZoomEnd;
+            controls.enableRotate = true;
 
-            camera.updateProjectionMatrix();
-
-            var temp = zoomPosStart;
-            zoomPosStart = zoomPosEnd;
-            zoomPosEnd = temp;
-
-            var temp = zoomViewStart;
-            zoomViewStart = zoomViewEnd;
-            zoomViewEnd = temp;
-
-            var temp = zoomZoomStart;
-            zoomZoomStart = zoomZoomEnd;
-            zoomZoomEnd = temp;
-
-            if (zoomIn) {
-                state = 'zoom2';
-            } else {
-                zoomIn = !zoomIn;
-            }
+            state = 'view';
+            zoomIn = !zoomIn;
         }
-    } else if (state == 'zoom2') {
-        zoomFrames ++;
-
-        zoomCounter = Math.sin(zoomFrames * Math.PI / (maxZoomFrames / 2));
-        camera.zoom += (zoom2End - zoom2Start) * zoomCounter * Math.PI / maxZoomFrames;
-        camera.updateProjectionMatrix();
-
-        if (zoomFrames >= maxZoomFrames / 2) {
-            zoomFrames = 0;
-
-            var temp = zoom2End;
-            zoom2End = zoom2Start;
-            zoom2Start = temp;
-
-            if (!zoomIn) {
-                state = 'zoom';
-            } else {
-                zoomIn = !zoomIn;
-            }
-        }
-    } else {
     }
-
+    else if (state == 'view') {
+    }
+    renderer.render( scene, camera );
     requestAnimationFrame( animate );
 	controls.update();
-
-    renderer.render( scene, camera );
 }
 animate();
